0f8936cff2dc2d35b47c0f5ae9fa3a3e9f35e11e
Vadim Gromov <vadimgromov@ukr.net>
2020-03-28 01:55:22 +0200

    Login into the site (update)

--- Student-project/Student-project/Controllers/LoginController.cs
+++ Student-project/Student-project/Controllers/LoginController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Student_project.Repository;
+
 
 namespace Student_project.Controllers
 {
     public class LoginController : Controller
     {
+        CDBContext db = new CDBContext();
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult LoginIntoSite(string UserName, string Password)
+        {
+            if(db.Students.Find(UserName) != null && Password == UserName)
+            {
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                return Content($"Невірний логін чи пароль");
+            }
         }
     }
 }
