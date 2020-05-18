5e2cbfed94e78bbc59085339c759919ffdadc442
Vadim Gromov <vadimgromov1403@gmail.com>
2020-05-18 15:36:25 +0300

    Add year select box and delete unnecessary files

--- Student-project/Student-project/Controllers/AdminController.cs
+++ Student-project/Student-project/Controllers/AdminController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Logging;
 using Student_project.Model;
 using Student_project.Repository;
