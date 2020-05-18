8cd2bd0eca0f0971cfd2f6fdafc67f4d4fd8caa2
Vadim Gromov <vadimgromov1403@gmail.com>
2020-05-18 05:29:53 +0300

    Exit from teacher page

--- Student-project/Student-project/Controllers/TeacherController.cs
+++ Student-project/Student-project/Controllers/TeacherController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,11 @@ namespace Student_project.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> Exit()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Login");
         }
     }
 }
